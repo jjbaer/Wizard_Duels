f0652d443175b4d41ec79df385239b3279d11e4c
Jenna Baer <jjbaer@calpoly.edu>
2017-01-17 16:02:55 -0800

    Completed basic iMessage stepper tutorial

--- MessagesExtension/MessagesViewController.swift
+++ MessagesExtension/MessagesViewController.swift
@@ -11,6 +11,52 @@ import Messages
 
 class MessagesViewController: MSMessagesAppViewController {
     
+    @IBOutlet weak var stepper: UIStepper!
+    
+    @IBAction func didPressCreate(_ sender: Any) {
+        if let image = createImageForMessage(), let conversation = activeConversation {
+            let layout = MSMessageTemplateLayout()
+            layout.image = image
+            layout.caption = "Stepper Value"
+            
+            let message = MSMessage()
+            message.layout = layout
+            message.url = URL(string: "emptyURL")
+            
+            conversation.insert(message, completionHandler: { (error: Error?) in
+                print(error ?? "I dunno")
+            })
+        }
+    
+    }
+    
+    func createImageForMessage() -> UIImage? {
+        let background = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+        background.backgroundColor = UIColor.white
+        
+        let label = UILabel(frame: CGRect(x: 75, y: 75, width: 150, height: 150))
+        label.font = UIFont.systemFont(ofSize: 56.0)
+        label.backgroundColor = UIColor.red
+        label.textColor = UIColor.white
+        label.text = "\(Int(stepper.value))"
+        label.textAlignment = .center
+        label.layer.cornerRadius = label.frame.size.width/2.0
+        label.clipsToBounds = true
+        
+        background.addSubview(label)
+        background.frame.origin = CGPoint(x: view.frame.size.width, y: view.frame.size.height)
+        view.addSubview(background)
+        
+        UIGraphicsBeginImageContextWithOptions(background.frame.size, false, UIScreen.main.scale)
+        background.drawHierarchy(in: background.bounds, afterScreenUpdates: true)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        background.removeFromSuperview()
+        
+        return image
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
