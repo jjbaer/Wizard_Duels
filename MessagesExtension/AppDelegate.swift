5de129962ca139e9a48a35eaf289df937badb365
Jenna Baer <jjbaer@calpoly.edu>
2017-01-26 12:08:46 -0800

    added storybord constraints, drawing still broken

---
+++ MessagesExtension/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Wizard_Duels
+//
+//  Created by Jenna on 1/26/17.
+//  Copyright © 2017 Jenna. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        application.applicationSupportsShakeToEdit = true
+        return true
+    }
+    
+}
