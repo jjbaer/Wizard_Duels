524bfe237ebde0423b86aa1d74e55c8fb3a814df
rspatric <roslyn.sierra@gmail.com>
2017-02-06 11:54:42 -0800

    Incorporate metal tutorial one in app.

--- MessagesExtension/MessagesViewController.swift
+++ MessagesExtension/MessagesViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Messages
+import Metal
+import QuartzCore
 
 class MessagesViewController: MSMessagesAppViewController {
     @IBOutlet weak var canvas: UIImageView!
@@ -15,6 +17,19 @@ class MessagesViewController: MSMessagesAppViewController {
     var swiped = false
     var color = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
     var gesture = "nothing yet"
+    var device: MTLDevice! = nil
+    var metalLayer: CAMetalLayer! = nil
+    //metal triangle
+    let vertexData:[Float] = [
+        0.0, 1.0, 0.0,
+        -1.0, -1.0, 0.0,
+        1.0, -1.0, 0.0]
+    var vertexBuffer: MTLBuffer! = nil
+    //rendering pipeline for shaders
+    var pipelineState: MTLRenderPipelineState! = nil
+    var commandQueue: MTLCommandQueue! = nil
+    
+    var timer: CADisplayLink! = nil
 
     @IBAction func didPressSend(_ sender: Any) {
         if let image = createImageForMessage(), let conversation = activeConversation {
@@ -78,6 +93,66 @@ class MessagesViewController: MSMessagesAppViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        device = MTLCreateSystemDefaultDevice()
+        
+        metalLayer = CAMetalLayer()
+        metalLayer.device = device
+        metalLayer.pixelFormat = .bgra8Unorm
+        metalLayer.framebufferOnly = true
+        metalLayer.frame = view.layer.frame
+        view.layer.addSublayer(metalLayer)
+        
+        let dataSize = vertexData.count * MemoryLayout.size(ofValue: vertexData[0])
+        vertexBuffer = device.makeBuffer(bytes: vertexData, length: dataSize, options: [])
+
+        let defaultLibrary = device.newDefaultLibrary()
+        let fragmentProgram = defaultLibrary!.makeFunction(name: "basic_fragment")
+        let vertexProgram = defaultLibrary!.makeFunction(name: "basic_vertex")
+        
+        let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
+        pipelineStateDescriptor.vertexFunction = vertexProgram
+        pipelineStateDescriptor.fragmentFunction = fragmentProgram
+        pipelineStateDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        
+        do {
+            try self.pipelineState = device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
+        } catch let pipelineError as NSError {
+            print("Failed to create pipeline state, error \(pipelineError)")
+        }
+        
+        commandQueue = device.makeCommandQueue()
+        
+        timer = CADisplayLink(target: self, selector: #selector(MessagesViewController.gameloop))
+        timer.add(to: RunLoop.main, forMode: RunLoopMode.defaultRunLoopMode)
+    }
+    
+    //metal render function
+    func render() {
+        let drawable = metalLayer.nextDrawable()!
+        
+        let renderPassDescriptor = MTLRenderPassDescriptor()
+        renderPassDescriptor.colorAttachments[0].texture = drawable.texture
+        renderPassDescriptor.colorAttachments[0].loadAction = .clear
+        renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 0.0, green: 104.0/255.0, blue: 5.0/255.0, alpha: 1.0)
+        
+        let commandBuffer = commandQueue.makeCommandBuffer()
+        
+        let renderEncoderOpt = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
+        
+        renderEncoderOpt.setRenderPipelineState(pipelineState)
+        renderEncoderOpt.setVertexBuffer(vertexBuffer, offset: 0, at: 0)
+        renderEncoderOpt.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3, instanceCount: 1)
+        renderEncoderOpt.endEncoding()
+        
+        commandBuffer.present(drawable)
+        commandBuffer.commit()
+    }
+    
+    //metal function
+    func gameloop() {
+        autoreleasepool {
+            self.render()
+        }
     }
     
     func drawLines(fromPoint: CGPoint, toPoint: CGPoint) {
---
+++ MessagesExtension/Shaders.metal
@@ -0,0 +1,22 @@
+//
+//  Shaders.metal
+//  Wizard_Duels
+//
+//  Created by Roslyn Patrick-Sunnes on 2/5/17.
+//  Copyright © 2017 Jenna. All rights reserved.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+//vertex shader
+vertex float4 basic_vertex(
+    const device packed_float3* vertex_array [[ buffer(0) ]],
+    unsigned int vid [[ vertex_id ]]) {
+    return float4(vertex_array[vid], 1.0);
+}
+
+//fragment shader
+fragment half4 basic_fragment() {
+    return half4(1.0);
+}
