c8f4b60b82172a3d12f7c953354c4f2bd6c73db1
Jenna <jenna.j.baer@gmail.com>
2017-05-03 15:48:05 -0700

    Added logic for p1 and p2 health (still not implemented but the structure is there) and got rounds to increment correctly

--- MessagesExtension/GameState.swift
+++ MessagesExtension/GameState.swift
@@ -12,15 +12,19 @@ class GameState {
     var currentTexture: String
     var currentPlayer: String
     var p1Move: String
+    var p1Health: String
+    var p2Health: String
     var p2Move: String
     var gameResult: String
     var round: Int
     
-    init(currentTexture: String, currentPlayer: String, p1Move: String, p2Move: String, gameResult: String, round: Int) {
+    init(currentTexture: String, currentPlayer: String, p1Move: String, p2Move: String, gameResult: String, round: Int, p1Health: String, p2Health: String) {
         self.currentTexture = currentTexture
         self.currentPlayer = currentPlayer
         self.p1Move = p1Move
         self.p2Move = p2Move
+        self.p1Health = p1Health
+        self.p2Health = p2Health
         self.gameResult = gameResult
         self.round = round
     }
@@ -48,7 +52,6 @@ class GameState {
             switch oponentMove {
             case "rock":
                 gameResult = "tied"
-                print("1")
             case "paper":
                 gameResult = "lost"
             case "scissors":
@@ -62,7 +65,6 @@ class GameState {
                 gameResult = "won"
             case "paper":
                 gameResult = "tied"
-                print("2")
             case "scissors":
                 gameResult = "lost"
             default:
@@ -76,7 +78,6 @@ class GameState {
                 gameResult = "won"
             case "scissors":
                 gameResult = "tied"
-                print("3")
             default:
                 gameResult = "incomplete"
             }
@@ -85,5 +86,20 @@ class GameState {
         }
         
         return gameResult
+    }
+    
+    // this one deals with converting strings --> ints and back
+    func calculateHealth() {
+        if (currentPlayer == "1" && gameResult == "lost") {
+            p1Health = String((Int(p1Health)! - 1))
+        }
+        else if (currentPlayer == "2" && gameResult == "lost") {
+            p2Health = String((Int(p2Health)! - 1))
+        }
+    }
+    
+    // next time: determine if there is a loser
+    func isLoser() -> Bool {
+        return false
     }
 }
